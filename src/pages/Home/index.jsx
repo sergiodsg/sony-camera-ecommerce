b9b15ce0e2f60d14af145fa3d9f5505d54dde9ba
Sergio Da Silva <sergiodasilva.dev@gmail.com>
2023-12-26 12:37:21 -0400

    Added ProductCard component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,40 @@
+import ProductCard from "../../components/ProductCard";
+
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="m-8">
+      <div className="flex flex-wrap justify-center">
+        <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 p-4">
+          <ProductCard />
+        </div>
+        <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 p-4">
+          <ProductCard />
+        </div>
+        <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 p-4">
+          <ProductCard />
+        </div>
+        <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 p-4">
+          <ProductCard />
+        </div>
+        <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 p-4">
+          <ProductCard />
+        </div>
+        <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 p-4">
+          <ProductCard />
+        </div>
+        <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 p-4">
+          <ProductCard />
+        </div>
+        <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 p-4">
+          <ProductCard />
+        </div>
+        <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 p-4">
+          <ProductCard />
+        </div>
+        <div className="sm:w-1/2 md:w-1/3 lg:w-1/4 p-4">
+          <ProductCard />
+        </div>
+      </div>
+    </div>
+  );
 }
