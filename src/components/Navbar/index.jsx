9f4e4c9506d43dfa5263b47b5a364ad4b457f1af
Sergio Da Silva <sergiodasilva.dev@gmail.com>
2024-01-03 11:34:38 -0400

    small styling changes

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -76,18 +76,12 @@ export default function Navbar() {
           className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block"
         >
           <div className="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
-            <a
-              className="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-              href="#"
-            >
-              Categories
-            </a>
             {/* Dropdown */}
             <div className="hs-dropdown relative inline-flex [--trigger:hover]">
               <button
                 id="hs-dropdown-hover-event"
                 type="button"
-                className="hs-dropdown-toggle py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium text-gray-600 hover:text-gray-400 disabled:opacity-50 disabled:pointer-events-none dark:text-gray-400 dark:hover:text-gray-500 dark:focus:outline-none"
+                className="hs-dropdown-toggle inline-flex items-center gap-x-1.5 text-sm font-medium text-gray-600 hover:text-gray-400 disabled:opacity-50 disabled:pointer-events-none dark:text-gray-400 dark:hover:text-gray-500 dark:focus:outline-none"
               >
                 Categories
                 <svg
@@ -107,7 +101,7 @@ export default function Navbar() {
               </button>
 
               <div
-                className="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-[15rem] bg-white shadow-md rounded-lg p-2 mt-2 dark:bg-gray-800 dark:border dark:border-gray-700 dark:divide-gray-700 after:h-4 after:absolute after:-bottom-4 after:start-0 after:w-full before:h-4 before:absolute before:-top-4 before:start-0 before:w-full"
+                className="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-[15rem] bg-white shadow-md rounded-lg p-2 mt-2 dark:bg-gray-800 dark:border dark:border-gray-700 dark:divide-gray-700 after:h-4 after:absolute after:-bottom-4 after:start-0 after:w-full before:h-4 before:absolute before:-top-4 before:start-0 before:w-full z-10"
                 aria-labelledby="hs-dropdown-hover-event"
               >
                 <a
@@ -149,7 +143,7 @@ export default function Navbar() {
             >
               Account
             </a>
-            <ToggleTheme />
+            <ToggleTheme/>
           </div>
         </div>
       </nav>
