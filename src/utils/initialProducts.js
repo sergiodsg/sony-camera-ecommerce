d2d10a562931bf174247a093850373431ff040b4
Sergio Da Silva <sergiodasilva.dev@gmail.com>
2024-01-08 11:32:06 -0400

    Added fake products in context

--- src/utils/initialProducts.js
+++ src/utils/initialProducts.js
@@ -1,112 +1,112 @@
 export const initialProducts = [
   {
     id: 1,
-    name: "Sony α6000",
-    price: 300,
+    name: "Sony α6700",
+    price: 399,
     category: "Cameras",
     description:
       "Some quick example text to build on the card title and make up the bulk of the card's content.",
     image:
-      "https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80",
-  },
-  {
-    id: 2,
-    name: "Sony α6000",
-    price: 300,
-    category: "Cameras",
-    description:
-      "Some quick example text to build on the card title and make up the bulk of the card's content.",
-    image:
-      "https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80",
+      "src/assets/Sony-6700.webp",
   },
   {
     id: 3,
-    name: "Sony α6000",
-    price: 300,
-    category: "Cameras",
+    name: "Dualsense Controller",
+    price: 89,
+    category: "Playstation",
     description:
       "Some quick example text to build on the card title and make up the bulk of the card's content.",
     image:
-      "https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80",
-  },
-  {
-    id: 4,
-    name: "Sony α6000",
-    price: 300,
-    category: "Cameras",
-    description:
-      "Some quick example text to build on the card title and make up the bulk of the card's content.",
-    image:
-      "https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80",
+      "src/assets/dualsense-controller.webp",
   },
   {
     id: 5,
-    name: "Sony α6000",
-    price: 300,
+    name: "Sony α7C",
+    price: 699,
     category: "Cameras",
     description:
       "Some quick example text to build on the card title and make up the bulk of the card's content.",
     image:
-      "https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80",
+      "src/assets/Sony-7C.webp",
   },
   {
     id: 6,
-    name: "Sony α6000",
-    price: 300,
-    category: "Cameras",
+    name: "Sony WF1000XM4",
+    price: 119,
+    category: "Audio",
     description:
       "Some quick example text to build on the card title and make up the bulk of the card's content.",
     image:
-      "https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80",
-  },
-  {
-    id: 7,
-    name: "Sony α6000",
-    price: 300,
-    category: "Cameras",
-    description:
-      "Some quick example text to build on the card title and make up the bulk of the card's content.",
-    image:
-      "https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80",
-  },
-  {
-    id: 8,
-    name: "Sony α6000",
-    price: 300,
-    category: "Cameras",
-    description:
-      "Some quick example text to build on the card title and make up the bulk of the card's content.",
-    image:
-      "https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80",
+      "src/assets/Sony-WF1000XM4.webp",
   },
   {
     id: 9,
-    name: "Sony α6000",
-    price: 300,
-    category: "Cameras",
+    name: "Sony WH1000XM5",
+    price: 249,
+    category: "Audio",
     description:
       "Some quick example text to build on the card title and make up the bulk of the card's content.",
     image:
-      "https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80",
+      "src/assets/Sony-WH1000XM5.avif",
   },
   {
-    id: 10,
-    name: "Sony α6000",
-    price: 300,
+    id: 8,
+    name: "Sony α9 III",
+    price: 1249,
     category: "Cameras",
     description:
       "Some quick example text to build on the card title and make up the bulk of the card's content.",
     image:
-      "https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80",
+      "src/assets/Sony-9III.webp",
   },
   {
     id: 11,
-    name: "Sony α6000",
-    price: 300,
+    name: "FE 15-35mm",
+    price: 499,
     category: "Cameras",
     description:
       "Some quick example text to build on the card title and make up the bulk of the card's content.",
     image:
-      "https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80",
-  }
+      "src/assets/FE 15-35mm.webp",
+  },
+  {
+    id: 7,
+    name: "Sony ZVE1",
+    price: 799,
+    category: "Cameras",
+    description:
+      "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    image:
+      "src/assets/Sony-ZVE1.webp",
+  },
+  {
+    id: 4,
+    name: "Sony 4K AX43A",
+    price: 449,
+    category: "Cameras",
+    description:
+      "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    image:
+      "src/assets/Handycam 4K AX43A.webp",
+  },
+  {
+    id: 2,
+    name: "Playstation 5",
+    price: 599,
+    category: "Playstation",
+    description:
+      "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    image:
+      "src/assets/ps5.webp",
+  },
+  {
+    id: 10,
+    name: "Sony α7CR",
+    price: 949,
+    category: "Cameras",
+    description:
+      "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    image:
+      "src/assets/Sony-7CR.webp",
+  },
 ];
