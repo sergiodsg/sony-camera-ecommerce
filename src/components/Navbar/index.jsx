6f3095866c61ad04f6f0dd6d894c3e57728119a9
Sergio Da Silva <sergiodasilva.dev@gmail.com>
2024-02-26 18:18:12 -0400

    first change

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -40,8 +40,8 @@ export default function Navbar() {
             <button
               type="button"
               className="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-x-2 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-transparent dark:border-gray-700 dark:text-white dark:hover:bg-white/10 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-              data-hs-collapse="#navbar-collapse-with-animation"
-              aria-controls="navbar-collapse-with-animation"
+              data-hs-collapse="#navbar-with-collapse"
+              aria-controls="navbar-with-collapse"
               aria-label="Toggle navigation"
             >
               <svg
@@ -79,8 +79,8 @@ export default function Navbar() {
           </div>
         </div>
         <div
-          id="navbar-collapse-with-animation"
-          className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block"
+          id="navbar-with-collapse"
+          className="hidden basis-full grow sm:block"
         >
           <div className="flex flex-col gap-8 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
             {/* Dropdown */}
